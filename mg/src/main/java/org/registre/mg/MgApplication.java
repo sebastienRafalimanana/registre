a368f7b378839fdcb6946e0f28efcc8dfd639810
Sebastien Rafalimanana <sebastienrafalimanana@gmail.com>
2023-07-15 15:05:09 +0300

    image preprocessing

---
+++ mg/src/main/java/org/registre/mg/MgApplication.java
@@ -0,0 +1,13 @@
+package org.registre.mg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgApplication.class, args);
+	}
+
+}
---
+++ mg/src/test/java/org/registre/mg/MgApplicationTests.java
@@ -0,0 +1,13 @@
+package org.registre.mg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
